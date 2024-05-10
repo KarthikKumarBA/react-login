1f0579cc0bdd84885b3c875cda5fdba75e1b3afb
KarthikKumarBA <bakarthikkumarba@gmail.com>
2024-05-10 14:42:51 +0530

    code

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,32 @@
-const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+require('dotenv').config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+
+
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "signup"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE
 })
+
+db.connect((err) => {
+  if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+  }
+  console.log('Connected to MySQL');
+});
 
 app.post('/signup', (req, res) => {
     const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
@@ -27,7 +43,10 @@ app.post('/signup', (req, res) => {
     })
 })
 app.post('/login', (req, res) => {
+
   const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?";
+
+
   db.query(sql, [req.body.email,req.body.password], (err, data) => {
     if (err) {
       return res.json("Error");
@@ -39,7 +58,19 @@ app.post('/login', (req, res) => {
       return res.json("Failed");
     }
   })
+  const [errors, setErrors] = useState({})
 })
+
+app.get('/loginData', (req, res) => {
+  const sql = "SELECT * FROM login";
+  db.query(sql, (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error retrieving data from the database' });
+    } 
+    return res.json(data);
+  });
+});
+
 
 
 app.listen(8081, () =>{
